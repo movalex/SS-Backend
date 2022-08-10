9508a8502a0e194dae8dfdf05b7c00c9ea02808d
Bruno Reis <brunocbreis@gmail.com>
2022-08-10 17:37:34 -0300

    moved concrete fusion api to main compscript

--- __init__.py
+++ __init__.py
@@ -3,6 +3,4 @@ from .defaults import DEFAULTS
 from .controller import Controller
 from .handler import EventHandler
 from .user_input import UserInput
-from .resolve_api import ResolveFusionAPI
 from .gui import ScreenSplitterGUI
-from .app import App
