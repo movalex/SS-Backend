ea4e92b0c0ebb8ec5efc157b3246fe72b9bf4bfd
Bruno Reis <brunocbreis@gmail.com>
2022-08-11 13:33:46 -0300

    created instructions file

---
+++ instructions.py
@@ -0,0 +1,4 @@
+DRAW_SCREEN = "Click and drag on the grid above to draw your first screen."
+DELETE_SCREEN = (
+    "Tip: you can delete any of your created screens by right-mouse-clicking on it."
+)
